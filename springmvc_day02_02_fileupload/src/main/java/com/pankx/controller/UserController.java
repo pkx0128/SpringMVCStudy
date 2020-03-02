17242bb3781db5ef7637fb1c02982514742665f7
pkx0128 <764670547@qq.com>
2020-03-02 22:44:02 +0800

    SpirngMVC实例上传文件

--- springmvc_day02_02_fileupload/src/main/java/com/pankx/controller/UserController.java
+++ springmvc_day02_02_fileupload/src/main/java/com/pankx/controller/UserController.java
@@ -6,9 +6,11 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -57,6 +59,22 @@ public class UserController {
             }
         }
 
+        return "success";
+    }
+
+    /**
+     * springmvc文件上传实例
+     * @return
+     */
+    @RequestMapping("/fileupload2")
+    public String fileload2(HttpServletRequest request,MultipartFile upload) throws IOException {
+        System.out.println("fileload2执行。。。");
+//        设置上传的位置
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+//        获取上传文件的名称
+        String fileName = upload.getOriginalFilename();
+//        完成文件上传
+        upload.transferTo(new File(path,fileName));
         return "success";
     }
 }
--- springmvc_day02_02_fileupload/target/springmvc_day02_02_fileupload/index.jsp
+++ springmvc_day02_02_fileupload/target/springmvc_day02_02_fileupload/index.jsp
@@ -16,5 +16,10 @@
             选择文件：<input type="file" name="upload"/><br/>
                     <input type="submit" value="上传"/>
         </form>
+
+        <form action="user/fileupload2" method="post" enctype="multipart/form-data">
+            选择文件：<input type="file" name="upload"/><br/>
+            <input type="submit" value="上传"/>
+        </form>
 </body>
 </html>
