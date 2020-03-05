02fffd6738925ad2111789b5bc30b25d58fcebe0
pkx0128 <764670547@qq.com>
2020-03-05 23:50:05 +0800

    Springmvc拦截器的简单实例

---
+++ springmvc_day02_04_interceptor/src/main/java/com/pankx/interceptor/MyInterceptor1.java
@@ -0,0 +1,31 @@
+package com.pankx.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor1 implements HandlerInterceptor {
+    /**
+     *预处理，controller方法执行前执行
+     * return true放行，执行下一个拦截器，如果没有拦截器，执行controller中的方法
+     * return false,不放行
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("MyInterceptor1拦截器执行了。。。。");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+}
