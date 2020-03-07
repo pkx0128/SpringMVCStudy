009f450fa39d57cfda5da3efefb78f4399d47f4e
pkx0128 <764670547@qq.com>
2020-03-07 11:58:32 +0800

    拦截器postHandle的简单用法

--- springmvc_day02_04_interceptor/src/main/java/com/pankx/interceptor/MyInterceptor1.java
+++ springmvc_day02_04_interceptor/src/main/java/com/pankx/interceptor/MyInterceptor1.java
@@ -20,12 +20,21 @@ public class MyInterceptor1 implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("MyInterceptor1拦截器执行了。。。。");
+        System.out.println("MyInterceptor1拦截器执行了。。。。前");
+//        request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request,response);
         return true;
     }
 
+    /**
+     * 后处理方法,controller方法执行之后执行
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
+        System.out.println("MyInterceptor1拦截器执行了。。。。后");
     }
 }
---
+++ springmvc_day02_04_interceptor/target/springmvc_day02_04_interceptor/WEB-INF/pages/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: pankx
+  Date: 2020/3/5
+  Time: 11:56 下午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <h1>Error</h1>
+</body>
+</html>
